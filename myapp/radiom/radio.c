d473af8aef220472c8ab649750e0c2e15767e802
KerisShu <58425243+KerisShu@users.noreply.github.com>
2021-05-22 18:38:25 +0800

    Add files via upload
    1.升级了报警机制；
    2.

--- myapp/radiom/radio.c
+++ myapp/radiom/radio.c
@@ -8,7 +8,7 @@
  * 2021-05-20     shangwang7       the first version
  */
 
-#include "myapp/radiom/radio.h"
+#include "radio.h"
 #include <rtthread.h>
 #include <rtdevice.h>
 
@@ -19,84 +19,102 @@
 #define THREAD_STACK_SIZE 512
 #define WARNING_THREAD_PRIORITY   30
 #define WARNING_TIMESLICE  50
+/* warning thread parameters*/
 
 
-static rt_thread_t ledwarning = RT_NULL;
-static rt_thread_t buzzerwarning = RT_NULL;
-int key_status;
+static rt_thread_t warning = RT_NULL;
+static rt_thread_t radio_acce = RT_NULL;
+
+
+static uint32_t thresh_val = 100; // 报警阈值
+static uint32_t current_val= 0;  // 当前辐射值
+
 
 
 /* led warning thread entry function */
-static void led_warning(void *param)
+static void warning_func(void *param)
 {
-    rt_pin_mode(LED_WARN_PIN , PIN_MODE_OUTPUT);
+    rt_pin_mode(LED_WARN_PIN, PIN_MODE_OUTPUT);
+    int status;
+
     while(1)
     {
-        rt_pin_write(LED_WARN_PIN, PIN_HIGH);
-        rt_thread_mdelay(2000);
-        rt_pin_write(LED_WARN_PIN, PIN_LOW);
-        rt_thread_mdelay(2000);
+        if (current_val >= thresh_val )
+        {
+            rt_pin_write(BUZZER_PIN, PIN_HIGH);
+            status = rt_pin_read(rt_pin_read(BUZZER_PIN));
+            rt_kprintf("BUZZER_HIGH: %x\n",status);
+            rt_kprintf("\\  ||  /\n");
+            rt_kprintf(" -----\n");
+            rt_kprintf(" \\   /\n");
+            rt_kprintf("  \\ /\n");
+            rt_kprintf("   -\n");
+
+            rt_pin_write(LED_WARN_PIN, PIN_HIGH);
+
+            rt_thread_mdelay(500);
+
+            rt_pin_write(BUZZER_PIN, PIN_LOW);
+            status = rt_pin_read(rt_pin_read(BUZZER_PIN));
+            rt_kprintf("BUZZER_HIGH: %x\n",status);
+            rt_pin_write(LED_WARN_PIN, PIN_LOW);
+
+            rt_thread_mdelay(500);
+        }
     }
 }
 
 /* buzzer warning thread entry function */
-static void buzzer_warning(void *param)
+static void radio_acce_f(void *param)
 {
-    rt_pin_mode(BUZZER_PIN , PIN_MODE_OUTPUT);
-    rt_pin_write(BUZZER_PIN, PIN_LOW);
+    rt_pin_mode(KEY_RADIO_PIN , PIN_MODE_INPUT);// 模拟达到报警阈值
 
-    rt_pin_mode(KEY_BUZZER_PIN , PIN_MODE_INPUT);
-    int status;
-    rt_uint16_t anti_shake = 0; // count variable
+    int key_status;
+    key_status = rt_pin_read(KEY_RADIO_PIN);
+    rt_kprintf("KEY: %x\n",key_status);
 
     while(1)
     {
-        key_status = rt_pin_read(KEY_BUZZER_PIN);
+        key_status = rt_pin_read(KEY_RADIO_PIN);
         //rt_kprintf("KEY: %x",key_status);
-
 
         if (key_status == PIN_LOW) // press button: 0
         {
-            rt_thread_mdelay(10);// anti shake
-            anti_shake++;
-
-            //rt_kprintf("KEY: %x",key_status);
-            if (anti_shake == 200) // 2 seconds
-            {
-                rt_pin_write(BUZZER_PIN, PIN_HIGH);
-                status = rt_pin_read(rt_pin_read(BUZZER_PIN));
-                rt_kprintf("BUZZER_HIGH: %x\n",status);
-            }
+            rt_thread_mdelay(100);// anti shake
+            current_val = current_val + 10;
+            key_status = rt_pin_read(KEY_RADIO_PIN);
+            rt_kprintf("KEY: %x,%d\n",key_status,current_val/10);
         }
         else if(key_status != PIN_LOW)
         {
-            anti_shake = 0;
+            //current_val = 0;
         }
     }
 }
+
 
 /* warning thread */
 int warning_thread(void)
 {
     /* led warning thread */
-    ledwarning = rt_thread_create("led_w_t",
-                            led_warning, RT_NULL,
+    warning = rt_thread_create("warning_thread",
+                            warning_func, RT_NULL,
                             THREAD_STACK_SIZE,
                             WARNING_THREAD_PRIORITY, WARNING_TIMESLICE);
-    if (ledwarning != RT_NULL)
-        rt_thread_startup(ledwarning);
+    if (warning != RT_NULL)
+        rt_thread_startup(warning);
     else
-        rt_kprintf("Failed: led warning start ");
+        rt_kprintf("Failed: warning start ");
 
     /* buzzer warning thread */
-    buzzerwarning = rt_thread_create("buzzer_w_t",
-                            buzzer_warning, RT_NULL,
+    radio_acce = rt_thread_create("radio_acce",
+                            radio_acce_f, RT_NULL,
                             THREAD_STACK_SIZE,
                             WARNING_THREAD_PRIORITY, WARNING_TIMESLICE);
-    if (buzzerwarning != RT_NULL)
-        rt_thread_startup(buzzerwarning);
+    if (radio_acce != RT_NULL)
+        rt_thread_startup(radio_acce);
     else
-          rt_kprintf("Failed: buzzer warning start ");
+          rt_kprintf("Failed: radio_acce start ");
 
     return 0;
 }
